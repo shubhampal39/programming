e01299bfe4fb9f5b97aaaf267779efb7112ffdbc
shubhampal39 <40192314+shubhampal39@users.noreply.github.com>
2019-08-06 23:21:10 +0530

    Create bubblesort.cpp
    https://www.geeksforgeeks.org/cpp-program-for-bubble-sort/

---
+++ gfg/Sudo_Placement_19/sorting/bubblesort.cpp
@@ -0,0 +1,75 @@
+{
+//Initial Template for C
+// C program for implementation of Bubble sort
+#include <stdio.h>
+ 
+void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+void bubble(int arr[], int i, int n);
+ 
+// A function to implement bubble sort
+void bubbleSort(int arr[], int n)
+{
+   int i, j;
+   for (i = 0; i < n-1; i++)      
+        bubble(arr, i, n);
+}
+ 
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("
+");
+}
+ 
+// Driver program to test above functions
+int main()
+{
+    int arr[1000],n,T,i;
+  
+    scanf("%d",&T);
+    
+    while(T--){
+        
+    scanf("%d",&n);
+    
+    for(i=0;i<n;i++)
+      scanf("%d",&arr[i]);
+      
+    bubbleSort(arr, n);
+    printArray(arr, n);
+    }
+    return 0;;
+}
+}
+/*This is a function problem.You only need to complete the function given below*/
+//User function Template for C
+/* The task is to complete bubble() which is used 
+   in below bubbleSort() */
+/* Function to sort an array in ascending order using
+   bubble sort
+void bubbleSort(int arr[], int n)
+{
+   int i;
+   for (i = 0; i < n-1; i++)      
+        bubble(arr, i, n);
+} */
+void bubble(int arr[], int i, int n)
+{
+  for(int j=0;j<n-i-1;j++)
+  {
+      if(arr[j]>arr[j+1])
+      {
+          int t=arr[j];
+          arr[j]=arr[j+1];
+          arr[j+1]=t;
+      }
+  }
+}
